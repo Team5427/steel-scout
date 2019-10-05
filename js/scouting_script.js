46d1fcc7294ba90e87e66c2d11770f6dad41a803
Abinavraj5427 <abinavraj10@gmail.com>
2019-10-05 17:29:08 -0500

    Almost done with error handling. Just trying to solve the number val crisis

--- js/scouting_script.js
+++ js/scouting_script.js
@@ -2,6 +2,7 @@ function loadForm(){
     $.getJSON('./scouting-form.json', function(data) {
         formElements = data.items;
         var container = document.getElementById("formContainer");
+        var id = 0;
         formElements.forEach(element => {
             switch(element.type){
                 case "int":
@@ -13,6 +14,7 @@ function loadForm(){
                     input.classList.add("scouting_input");
                     input.type = "number";
                     input.name = element.field;
+                    input.id = element.field;
                     container.appendChild(input);
                     container.appendChild(document.createElement("br"));
                     break;
@@ -32,6 +34,7 @@ function loadForm(){
                     label.classList.add("switch");
                     var input  = document.createElement("input");
                     input.type = "checkbox";
+                    input.id = element.field;
                     var span = document.createElement("span");
                     span.classList.add("slider");
                     label.append(input);
@@ -76,6 +79,7 @@ function loadForm(){
                         var input = document.createElement("input");
                         input.type = "radio";
                         input.value = option;
+                        input.id = option;
                         input.name = element.field;
                         container.appendChild(input);
                         container.appendChild(document.createElement("br"));
@@ -90,6 +94,7 @@ function loadForm(){
                     var input = document.createElement("input");
                     input.classList.add("scouting_input");
                     input.type = "text";
+                    input.id = element.field;
                     container.appendChild(input);
                     break;
                 default:
@@ -98,82 +103,91 @@ function loadForm(){
         })
     });
 }
- 
-$("#scouting_submit").click(function(){ 
-	event.preventDefault();
-	var error_msg = ''; 
-	$("#scouting_error_message").html(error_msg); 
 
-	//reset the color of all the inputs in case there was an error
-    $(".scouting_label").css('color', 'black');
-    
-    error_msg = 'The following mandatory fields are incomplete: '; 
-    error_exists = false;
-    $.getJSON('./scouting-form.json', function(data) {
-        formElements = data.items;
-        formElements.forEach(element => {
-            if(element.type === "int" || element.type === "text"){
-                error_exists = true;
-                if($("#"+element.field).val() == ''){
-                    error_msg += '</br>&nbsp;&nbsp;&nbsp;&nbsp;'+element.field; 
-			        $("#"+element.field).css('color', '#ef2323');
-                }
-            }
-            else if(element.type === "MC"){
-                if($('input[name='+ element.field+']:checked').length <= 0){
-                    error_exists = true;
-                    error_msg += '</br>&nbsp;&nbsp;&nbsp;&nbsp;'+element.field; 
-			        $("#"+element.field).css('color', '#ef2323');
-                }
-            }
-        });
+function getFields(callback){
+    $.getJSON('./scouting-form.json',function (data) {
+        //execute the callback, passing it the data
+        callback(data);
     });
-    if(error_exists){
+}
+
+$(document).ready(function() {
+    $("#scouting_submit").click(function(){ 
+        event.preventDefault();
+        var error_msg = ''; 
         $("#scouting_error_message").html(error_msg); 
-		window.scrollTo(0, 0);
-    }
-	else
-	{
-        resp = [];
-        $.getJSON('./scouting-form.json', function(data) {
+
+        //reset the color of all the inputs in case there was an error
+        $(".scouting_label").css('color', 'black');
+        
+        error_msg = 'The following mandatory fields are incomplete: '; 
+        error_exists = false;
+        getFields(function(data) {
             formElements = data.items;
             formElements.forEach(element => {
-                if(element.type == "int" || element.type == "text"){
-                    resp.append($("#"+element.field).val());
+                console.log(element.field);
+                if(element.type === "int" || element.type === "text"){
+                    console.log(element.field + ": "+ $("#"+element.field).val());
+                    if($("#"+element.field).val() === '' || $("#"+element.field).val() === undefined){
+                        error_exists = true;
+                        error_msg += '</br>&nbsp;&nbsp;&nbsp;&nbsp;'+element.field; 
+                        $("#"+element.field).css('color', '#ef2323');
+                    }
                 }
-                else if(element.type == "MC"){
-                    element.options.forEach(option => {
-                        if(document.getElementById(option).checked)
-                            resp.append(option);
-                    });
-                }
-                else if(element.type == "switch"){
-                    if($("#"+element.field).checked) resp.append(element.options[1]);
-                    else resp.append(element.options[0]);
+                else if(element.type === "MC"){
+                    if($('input[name='+ element.field+']:checked').length <= 0){
+                        error_exists = true;
+                        error_msg += '</br>&nbsp;&nbsp;&nbsp;&nbsp;'+element.field; 
+                        $("#"+element.field).css('color', '#ef2323');
+                    }
                 }
             });
-        });
-
-	    $.ajax({
-		    url:'http://127.0.0.1:8090/steel-scout-middleend/scouting.php',
-		    data: {values: resp},
-		    type: "POST", //or type:"GET" or type:"PUT"
-		    success: function (result) {
-		    	console.log(result); 
-		    },
-		    error: function(jqXHR, textStatus, errorThrown, result) {
-	                alert('An error occurred... Look at the console (F12 or Ctrl+Shift+I, Console tab) for more information!');
-
-	                $('#result').html('<p>status code: '+jqXHR.status+'</p><p>errorThrown: ' + errorThrown + '</p><p>jqXHR.responseText:</p><div>'+jqXHR.responseText + '</div>');
-	                console.log('jqXHR:');
-	                console.log(jqXHR);
-	                console.log('textStatus:');
-	                console.log(textStatus);
-	                console.log('errorThrown:');
-	                console.log(errorThrown);
-	                console.log('JSON return string: '); 
-	                console.log(result); 
-	            },
-		});
-	}
+            
+            if(error_exists){
+                $("#scouting_error_message").html(error_msg); 
+                window.scrollTo(0, 0);
+            }
+            else
+            {
+                resp = [];
+                formElements.forEach(element => {
+                    if(element.type === "int" || element.type === "text"){
+                        resp.push($("#"+element.field).val());
+                    }
+                    else if(element.type == "MC"){
+                        element.options.forEach(option => {
+                            if(document.getElementById(option).checked)
+                                resp.push(option);
+                        });
+                    }
+                    else if(element.type == "switch"){
+                        if($("#"+element.field).checked) resp.push(element.options[1]);
+                        else resp.push(element.options[0]);
+                    }
+                });
+        
+                $.ajax({
+                    url:'http://127.0.0.1:8090/steel-scout-middleend/scouting.php',
+                    data: {values: resp},
+                    type: "POST", //or type:"GET" or type:"PUT"
+                    success: function (result) {
+                        console.log(result); 
+                    },
+                    error: function(jqXHR, textStatus, errorThrown, result) {
+                            alert('An error occurred... Look at the console (F12 or Ctrl+Shift+I, Console tab) for more information!');
+        
+                            $('#result').html('<p>status code: '+jqXHR.status+'</p><p>errorThrown: ' + errorThrown + '</p><p>jqXHR.responseText:</p><div>'+jqXHR.responseText + '</div>');
+                            console.log('jqXHR:');
+                            console.log(jqXHR);
+                            console.log('textStatus:');
+                            console.log(textStatus);
+                            console.log('errorThrown:');
+                            console.log(errorThrown);
+                            console.log('JSON return string: '); 
+                            console.log(result); 
+                        },
+                });
+            }
+        });    
+    });
 });
