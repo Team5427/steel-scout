ae7a4bf76c5251c85597bba672803144d2ff37e6
ujjainidas <43097770+ujjainidas@users.noreply.github.com>
2020-01-26 15:11:54 -0600

    Frontend created, login system needs testing

---
+++ php/config.php
@@ -0,0 +1,13 @@
+<?php
+session.start();
+$host = "localhost";
+$user = "steel_scout";
+$password = "!te@m5427!";
+$dbname = "steel_scout";
+
+$con = mysqli_connect($host, $user, $password, $dbname);
+
+if(!$con)
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
