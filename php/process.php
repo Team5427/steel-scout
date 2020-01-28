fdeaab087c4ffd0208a468563abeecf41b4697b1
Abinavraj5427 <abinavraj10@gmail.com>
2020-01-28 08:05:12 -0600

    Confirm Login works successfully. Auth token being stored correctly

--- php/process.php
+++ php/process.php
@@ -10,7 +10,7 @@
     mysql_connect("localhost", "steel_scout", "!te@m5427!");
     mysql_select_db("steel-scout");
 
-    $result = mysql_query("select * from users where username = '$username' and password = '$password'");
+    $result = mysql_query("select * from users where username = '$username' and password = '$password'")
             or die("Failed to query database ".mysql_error());
     $row = mysql_fetch_array($result);
     if(row['username'] == $username && row['password'] == $password)
