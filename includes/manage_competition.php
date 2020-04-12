de6b67e3663b990c2d7b587efd0ef8611998faa1
Savvy Chez <savvychez@gmail.com>
2020-04-11 19:55:36 -0500

    Completed add competitions and manage competitions functionality

--- includes/manage_competition.php
+++ includes/manage_competition.php
@@ -10,7 +10,7 @@
 <?php
     
     //looking for all users username
-    $sql = "SELECT * FROM 2020_competitions";
+    $sql = "SELECT * FROM 2020_competitions ORDER BY competition_name";
     $res = mysqli_query($connection, $sql);
 
 
@@ -18,6 +18,12 @@
     //collects all users
     if(mysqli_num_rows($res)>0){
       while($row = mysqli_fetch_assoc($res)){
+
+        $id = $row['season_id'];
+        $sql = "SELECT season_name FROM seasons WHERE season_id=$id";
+        $idres = mysqli_query($connection, $sql);
+        $row['season_name'] = mysqli_fetch_assoc($idres)['season_name'];
+
         array_push($rows, $row);
       }
     }
--- public/js/add_competition_script.js
+++ public/js/add_competition_script.js
@@ -1,22 +1,54 @@
+$(window).on('load', function () {
+	loadSeasons();
+})
+
+function loadSeasons() {
+	$.ajax({
+		url:'../includes/add_competition.php',
+		data: {sender: "loading"},
+		type: "POST",
+		success: function (result) {
+			result = JSON.parse(result);
+			$.each(result.seasons, function( index, value ) {
+				var x = value.season_name;
+				$("#seasons").append("<option id=\""+ value.season_id +"\" value=\""+ value.season_name + "\"/>");
+			});
+		},
+		error: function(xhr, status, error) {
+			var err = eval("(" + xhr.responseText + ")");
+			alert(err.Message);
+		}
+	});
+}
+
+
 function addComp() {
     event.preventDefault();
     var name = $("#name").val()
     var date = $("#date").val()
     var season = $("#season").val()
-
+	var season_id = $("#seasons option[value='" + season + "']").attr('id');
     $.ajax({
 		url:'../includes/add_competition.php',
-		data: {name: name, date: date, season: season},
+		data: {name, date, season_id, sender: "adding"},
 		type: "POST",
 		success: function (result) {
+			result = JSON.parse(result);
 			console.log(result);
+			window.location.assign("manage_competitions.html")
 		},
-		error: error()
+		error: function(xhr, status, error) {
+			var err = eval("(" + xhr.responseText + ")");
+			alert(err.Message);
+		}
 	});
-
     return false;
 }
 
 function error() {
-	alert("Yikes")
+	alert("Error!");
+}
+
+function cancelled() {
+	window.location.assign("manage_competitions.html");
 }
