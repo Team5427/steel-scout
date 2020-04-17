4f7c305b20cbdcdde6ce4f8ea1e7e4b88f4278f8
ujjainidas <43097770+ujjainidas@users.noreply.github.com>
2020-04-17 18:33:19 -0500

    team_id working with the INSERT function, changed to remove other fields to let victoria and rohini complete the rest

--- includes/new_scouting.php
+++ includes/new_scouting.php
@@ -8,31 +8,16 @@
 
 
 <?php
-    $team_id = $_POST['team_id'];
-    $scouter_id = $_POST['scouter_id'];
-    $competition_id = $_POST['competition_id'];
-//    $matchNumber = $_POST['matchNumber'];
-//    $powerCellsHighOne = $_POST['powerCellsHighOne'];
-//    $powerCellsLowOne = $_POST['powerCellsLowOne'];
-//    $radio1 = $_POST['radio1'];
-//    $powerCellsHighTwo = $_POST['powerCellsHighTwo'];
-//    $powerCellsLowTwo = $_POST['powerCellsLowTwo'];
-//    $radio2 = $_POST['radio2'];
-//    $powerCellsHighThree = $_POST['powerCellsHighThree'];
-//    $powerCellsLowThree = $_POST['powerCellsLowThree'];
-//    $radio3 = $_POST['radio3'];
-//    $radio4 = $_POST['radio4'];
-//    $radio5 = $_POST['radio5'];
-//    $finalRP = $_POST['finalRP'];
-//    $radio6 = $_POST['radio6'];
-//    $radio7 = $_POST['radio7'];
+    $team_id = $_POST['team'];
+//    $scouter_id = $_POST['scouter_id'];
+//    $competition_id = $_POST['competition_id'];
 
     if($connection -> connect_error)
     {
         die("Connection failed: " . $connection -> connect_error);
     }
 
-    $sql = "INSERT INTO 2020_scouting (team_id, scouter_id, competition_id) VALUES ($team_id, $scouter_id, $competition_id)";
+    $sql = "INSERT INTO 2020_scouting (team_id) VALUES ($team_id)";
 
     if($connection->query($sql) === true)
     {
--- public/js/new_scouting.js
+++ public/js/new_scouting.js
@@ -1,106 +1,35 @@
-//// //auto login code
-//// const numInputs = 16;
-//$(window).on('load', function() {
-//	let token = getCookie("token");
-//	// console.log("COOKIE: "+document.cookie);
-//	// console.log("TOKEN: "+token);
-//	if(token != null) {
-//		$.ajax({
-//			url:'http://localhost/steel-scout-frontend/php/confirmlogin.php',
-//			data: {token: token},
-//			type: "POST", //or type:"GET" or type:"PUT"
-//			success: function (result) {
-//				//console.log("RESULT: "+result);
-//				result = JSON.parse(result);
-//				if(!result['authenticated'] || result['role'] !== 'SCOUTER') 
-//					window.location.assign("http://localhost/steel-scout-frontend/login.html");
-//			},
-//			error: error()
-//		});
-//    }
-//    else{
-//        window.location.assign("http://localhost/steel-scout-frontend/login.html");
-//    }
-//});
-
 $("#scouting_submit").click(function()
 {
 	var check = true;
 	var numInputs = 1;
 	console.log("clicked");
 	event.preventDefault();
+	window.scrollTo(0,0);
+	$.ajax({
+            url: 'http://localhost/steel-scout/includes/new_scouting.php', 
+	    data:{
+                team: $('[name=team]').val()
+            }, 
+            type: "POST", 
+            success: function(result)
+            {
+		if(!result === "logged")
+                    $("#scouting_error_message").html(result.error);
+		else
+		{
+		    document.getElementById("scoutingForm").reset();
 
-//	$("input:radio").each(function(){
-//		var name = $(this).attr("name");
-//		if($("input:radio[name="+name+"]:checked").length == 0)
-//		{
-//			check = false;
-//			console.log(name + "is empty");
-//			$("#"+name+"id").css("color", "#ef2323");
-//		}
-//		else
-//		{
-//			$("#"+name+"id").css("color", "black");
-//		}
-//	});
-//
-//	for(var i = 1; i<=numInputs; i++)
-//	{
-//		var input = document.getElementById("input"+i);
-//
-//		if(input.type == "text" || input.type == "number")
-//		{
-//			if(input.value == '')
-//			{
-//				check = false
-//			    console.log("input"+i+" is empty");
-//				input.style.backgroundColor = "#ef2323";
-//			}
-//			else
-//			{
-//				input.style.backgroundColor = "#f1f1f1"
-//			}
-//		}
-//	}
-//	console.log(check);
-//	if(check === false)
-//	{
-//		$("#scouting_error_message").html("Some fields are incomplete: ");
-//		window.scrollTo(0, 0);
-//	}
-//	else
-//	{
-		window.scrollTo(0,0);
-		$.ajax({
-			url: 'http://localhost/steel-scout/includes/new_scouting.php', 
-			data: {team_id: $('[name=team_id]').val(), scouter_id: $('[name=scouter_id]').val(), competition_id: $('[name=competition_id]').val()
-//                                , match_number: $('[name=matchNumber]').val(), 
-//				powerCellsHighOne: $('[name=powerCellsHighOne]').val(), powerCellsLowOne: $('[name=powerCellsLowOne]').val(), radio1: $('input[type=radio][name=radio1]:checked').val(),
-//				powerCellsHighTwo: $('[name=powerCellsHighTwo]').val(), powerCellsLowTwo: $('[name=powerCellsLowTwo]').val(), radio2: $('input[type=radio][name=radio2]:checked').val(), 
-//				powerCellsHighThree: $('[name=powerCellsHighThree]').val(), powerCellsLowThree: $('[name=powerCellsLowThree]').val(), radio3: $('input[type=radio][name=radio3]:checked').val(), 
-//				radio4: $('input[type=radio][name=radio4]:checked').val(), radio5: $('input[type=radio][name=radio5]:checked').val(), finalRP: $('[name=finalRP]').val(), 
-//				radio6: $('input[type=radio][name=radio6]').val(), radio7: $('input[type=radio][name=radio7]').val()
-                            }, 
-			type: "POST", 
-			success: function(result)
-			{
-				if(!result === "logged")
-					$("#scouting_error_message").html(result.error);
-				else
-				{
-					document.getElementById("scoutingForm").reset();
-
-//					(result['role'] === "SCOUTER") && window.location.assign("http://"+ip+"/steel-scout-frontend/scouting.html");
-				}
+//		    (result['role'] === "SCOUTER") && window.location.assign("http://"+ip+"/steel-scout-frontend/scouting.html");
+		}
 				
-			}, 
-			error: function(jqXHR, textStatus, errorThrown, result)
-			{
-                            console.log("not sent");
-			    error(jqXHR, textStatus, errorThrown, result);
-			}
-		});
-	}
+	    }, 
+	    error: function(jqXHR, textStatus, errorThrown, result)
+	    {
+                console.log("not sent");
+                error(jqXHR, textStatus, errorThrown, result);
+	    }
+        });
+    }
 );
 
 function error(jqXHR, textStatus, errorThrown, result) {
