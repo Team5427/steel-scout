a191f86402aee3fb3c6934afb2586d0e10b99015
Sai Charan <tsaicharan03@gmail.com>
2020-06-18 10:55:24 -0500

    adding has errors

--- includes/add_competition.php
+++ includes/add_competition.php
@@ -2,26 +2,31 @@
 
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: *");
+?>
 
-    require_once("db_connection.php"); 
+   <?php require_once("db_connection.php"); ?>
 
-   $name = $_POST['name'];
-    // $date = $_POST['date'];
-    //looking for all users username
-    $sql = "SELECT * FROM 2020_competitions WHERE competition_name = \"$name\"";
-    $res = mysqli_query($connection, $sql);
+   <?php 
+        $name = $_POST['name'];
+        $date = $_POST['date'];
 
-    //collects all users
-    if(mysqli_num_rows($res)>0){
-        $response = array('success' => false, 'error' => "Duplicate value entered.");
-        echo json_encode($response);
-    }
-    else{
-        //looking for matching user
-        $sql = "INSERT INTO 2020_competitions (competition_name) VALUES (\"$name\")";
-        $res = mysqli_query($connection, $sql);
 
-        $response = array('success' => true);
-        echo json_encode($response);
-    }
- ?>
+        if($connection -> connect_error)
+        {
+            die("Connection failed: " . $connection -> connect_error);
+        }
+
+         $sql = "INSERT INTO 2020_competitions (competition_name,competition_date) VALUES (\"$name\",$date)";
+
+        if($con->query($sql) === true)
+        {
+            echo "logged";
+        }
+        else
+        {
+            echo "Error: " . $sql . "<br>" . $con->error;
+        }
+
+        mysqli_close($connection);
+    ?>
+            
--- public/js/add_competition_script.js
+++ public/js/add_competition_script.js
@@ -1,30 +1,55 @@
+$("#scouting_clear").click(function () {
+    document.getElementById("scoutingForm").reset();
+    console.log("cleared");
+
+});
+
+window.onload = function () {
+    document.getElementById("pit_submit").onclick = function () {
+
+        var name = document.getElementById("name").value;
+        var date = document.getElementById("date").value;
+        // var season = document.getElementById("season").value;
+
+        console.log(name);
+        console.log(date);
+
+
+        $.ajax({
+            url: "../includes/pitScouting.php",
+            data: { name,date},
+            type: "POST",
+            success: function (result) {
+                console.log(result);
+            },
+            error: function (result) {
+                console.log("error");
+            }
+        });
+        return false;
+    };
+}
+
+
+function error(jqXHR, textStatus, errorThrown, result) {
+    console.log("FAILURE");
+    alert('An error occurred... Look at the console (F12 or Ctrl+Shift+I, Console tab) for more information!');
+
+    $('#result').html('<p>status code: </p><p>errorThrown: ' + errorThrown + '</p><p>jqXHR.responseText:</p><div></div>');
+    console.log('jqXHR:');
+    console.log(jqXHR);
+    console.log('textStatus:');
+    console.log(textStatus);
+    console.log('errorThrown:');
+    console.log(errorThrown);
+    console.log('JSON return string: ');
+    console.log(result);
+}
 
 function getCookie(name) {
     var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
     return v ? v[2] : null;
 }
-
-function addComp(){
-    var name = document.getElementById("name").value;
-	var date = document.getElementById("date").value;
-	 var season = document.getElementById("season").value;
-    $.ajax({
-        url:'http://localhost/steel-scout/includes/add_competition.php',
-        data: {name, date},
-        type: "POST", //or type:"GET" or type:"PUT"
-        success: function (result) {
-			console.log("alsdjfl")
-            result = JSON.parse(result);
-			if(result.success)
-                window.location.assign("./manage_competitions.html");
-            else
-                document.getElementById("add_competition_error").innerHTML = "Failed to add a competition: "+result.error;
-        },
-        // error: error(error)
-    });
-    return false;
-}
-
-function cancel(){
-    window.location.assign("./manage_competitions.html");
-}
+function cancelled() {
+    window.location.assign("manage_competitions.html");
+} 
