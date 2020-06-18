ddae4897aa6ac095110a3b6355d3445587684dd0
Gauravaram Ganesh Sudhakar <gauravaram@live.com>
2020-06-17 19:57:33 -0500

    found a problem with add competitions only fixed name

--- includes/add_competition.php
+++ includes/add_competition.php
@@ -1,39 +1,27 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-?>
 
-<?php require_once("../includes/db_connection.php"); ?>
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
 
-<?php
-$data = array();
+    require_once("db_connection.php"); 
 
-if ($_POST['sender'] == "loading") {
-    $sql = "SELECT * FROM seasons";
+   $name = $_POST['name'];
+    // $date = $_POST['date'];
+    //looking for all users username
+    $sql = "SELECT * FROM 2020_competitions WHERE competition_name = \"$name\"";
     $res = mysqli_query($connection, $sql);
 
-    $rows = array();
+    //collects all users
     if(mysqli_num_rows($res)>0){
-      while($row = mysqli_fetch_assoc($res)){
-        array_push($rows, $row);
-      }
-      $data['seasons'] = $rows;
+        $response = array('success' => false, 'error' => "Duplicate value entered.");
+        echo json_encode($response);
     }
+    else{
+        //looking for matching user
+        $sql = "INSERT INTO 2020_competitions (competition_name) VALUES (\"$name\")";
+        $res = mysqli_query($connection, $sql);
 
-} else {
-    $name = $_POST['name'];
-    $date = $_POST['date'];
-    $season = $_POST['season_id'];
-
-    $sql = "INSERT INTO 2020_competitions (competition_name, competition_date, season_id) VALUES (\"$name\",\"$date\",\"$season\")";
-    $res = mysqli_query($connection, $sql);
-
-    $data['error'] = mysqli_error($connection);
-    $data['success'] = true;
-    $data['name'] = $name;
-    $data['date'] = $date;
-    $data['season'] = $season;
-}
-
-
-echo json_encode($data);
+        $response = array('success' => true);
+        echo json_encode($response);
+    }
+ ?>
--- public/js/add_competition_script.js
+++ public/js/add_competition_script.js
@@ -1,56 +1,30 @@
-$(window).on('load', function () {
-	loadSeasons();
-})
 
-function loadSeasons() {
-	$.ajax({
-		url:'../includes/add_competition.php',
-		data: {sender: "loading"},
-		type: "POST",
-		success: function (result) {
-			result = JSON.parse(result);
-			console.log(result);
-
-			$.each(result.seasons, function( index, value ) {
-				var x = value.season_name;
-				$("#seasons").append("<option id=\""+ value.season_id +"\" value=\""+ value.season_name + "\"/>");
-			});
-		},
-		error: function(xhr, status, error) {
-			var err = eval("(" + xhr.responseText + ")");
-			// alert(err.Message);
-		}
-	});
+function getCookie(name) {
+    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return v ? v[2] : null;
 }
 
-
-function addComp() {
-    event.preventDefault();
-    var name = $("#name").val()
-    var date = $("#date").val()
-    var season = $("#season").val()
-	var season_id = $("#seasons option[value='" + season + "']").attr('id');
+function addComp(){
+    var name = document.getElementById("name").value;
+	var date = document.getElementById("date").value;
+	 var season = document.getElementById("season").value;
     $.ajax({
-		url:'../includes/add_competition.php',
-		data: {name, date, season_id, sender: "adding"},
-		type: "POST",
-		success: function (result) {
-			result = JSON.parse(result);
-			console.log(result);
-			window.location.assign("manage_competitions.html")
-		},
-		error: function(xhr, status, error) {
-			var err = eval("(" + xhr.responseText + ")");
-			alert(err.Message);
-		}
-	});
+        url:'http://localhost/steel-scout/includes/add_competition.php',
+        data: {name, date},
+        type: "POST", //or type:"GET" or type:"PUT"
+        success: function (result) {
+			console.log("alsdjfl")
+            result = JSON.parse(result);
+			if(result.success)
+                window.location.assign("./manage_competitions.html");
+            else
+                document.getElementById("add_competition_error").innerHTML = "Failed to add a competition: "+result.error;
+        },
+        // error: error(error)
+    });
     return false;
 }
 
-function error() {
-	alert("Error!");
-}
-
-function cancelled() {
-	window.location.assign("manage_competitions.html");
+function cancel(){
+    window.location.assign("./manage_competitions.html");
 }
